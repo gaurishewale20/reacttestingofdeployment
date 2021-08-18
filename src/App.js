0559c79df676ad7266844ae7f50d56a1c1b47a2c
gaurishewale <gaurishewale201201@gmail.com>
2021-08-18 23:42:08 +0530

    Changed text to Japanase :)

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App ()
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi Everybody ! Gauri here
+          Konnichiwa Minasan! Gauri desu!
         </a>
       </header>
     </div>
